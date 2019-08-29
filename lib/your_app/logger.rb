ee7f2fc740bb9cfc29be4c06959d928b2e539ccf
Masafumi Koba <ybiquitous@gmail.com>
2019-08-29 10:18:41 +0900

    Fix deprecation warnings of Rails 6
    > DEPRECATION WARNING: Including LoggerSilence is deprecated and will be removed in Rails 6.1. Please use `ActiveSupport::LoggerSilence` instead
    
    > DEPRECATION WARNING: Logger don't need to call #after_initialize directly anymore. It will be deprecated without replacement in Rails 6.1.

--- lib/your_app/logger.rb
+++ lib/your_app/logger.rb
@@ -1,12 +1,7 @@
 module YourApp
   class Logger < Ougai::Logger
     include ActiveSupport::LoggerThreadSafeLevel
-    include LoggerSilence
-
-    def initialize(*args)
-      super
-      after_initialize if respond_to? :after_initialize
-    end
+    include ActiveSupport::LoggerSilence
 
     def create_formatter
       if Rails.env.development? || Rails.env.test?
